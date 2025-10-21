e6c9d67077b2093ecf2e3c6eddbb6629d6ba4d22
Kaktus1549 <hummelotup@seznam.cz>
2025-10-21 16:18:03 +0200

    Add max parameter handling to Home component for conditional rendering

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -2,11 +2,21 @@
 import Mainpage from "./component/_main";
 import Tututudu from "./component/_max";
 import Loading from "./component/_loading";
+import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [showMax, setShowMax] = useState(false);
   const [decided, setDecided] = useState(false);
+  let max: boolean | null = null ;
+
+  try{
+        const searchParams = useSearchParams();
+        max = searchParams.get('max') === 'true';
+    }
+    catch{
+        console.error("Error while parsing max :(");
+    }
 
   useEffect(() => {
     const random = Math.random();
@@ -17,6 +27,9 @@ export default function Home() {
   if (!decided) return(
     <Loading />
   )
+  if (max !== null && max === true) {
+    return <Tututudu />;
+  }
 
   return showMax ? <Tututudu /> : <Mainpage />;
 }
